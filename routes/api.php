deb3f9bce323a75cd45c5c9ff367698f7b41113f
AlphTheMech <76548040+AlphTheMech@users.noreply.github.com>
2022-02-06 16:23:51 +0400

    init

--- routes/api.php
+++ routes/api.php
@@ -19,36 +19,42 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 require __DIR__ . '/auth.php';
 // Route::post('/signin',[UserController::class, 'login']);
-Route::get('/dowload_file',[ImageController::class , 'donwload']);
-Route::middleware(['auth:sanctum'])->get('/user/info', [UserController::class, 'getInfoUser']);
-
+Route::get('/dowload_file', [ImageController::class, 'donwload']); //Скачивание файла
 // Route::get('/info', [UserController::class, 'getInfoUser']);//Информация об авторизированного пользователе
-Route::get('/find_for_admin', [UserController::class, 'findForAdmin']); //Поиск по почте
-Route::post('/create_subject', [UserController::class, 'createSubject']); //Добавление предмета
-Route::post('/create_test', [UserController::class, 'postTests']); //Добавление тестов 
-Route::post('create_new_expert', [UserController::class, 'createExpert']); //Выдача прав доступа "Эксперт"
-Route::post('create_new_teacher', [UserController::class, 'createTeacher']); //Выдача прав доступа "Учитель"
-Route::get('/all_tests', [UserController::class, 'getAllTests']); //Получение всех тестов
-Route::post('/give_acces_expert', [UserController::class, 'addingAccessToTest']); //Открытие доступа к тесту
-Route::middleware(['auth:sanctum'])->get('/find_tests', [UserController::class, 'searchForAnExpert']); //Получение тестов с открытым доступом
-Route::middleware(['auth:sanctum'])->post('/accept_result', [UserController::class, 'postResultTest']); //Добавление результатов прохождения теста
-Route::middleware(['auth:sanctum'])->post('/image', [ImageController::class, 'create']); //Загрузка фото
-Route::post('/login', [UserController::class, 'login']);//Авторизация
-Route::get('/get_all_expert', [UserController::class, 'getAllExpert']);//Получение всех экспертов
 // Route::post('/signin', [UserController::class, 'login']);
-// Route::group(['middleware' => 'role:user'], function( ){
-//     Route::post('/signin', [UserController::class, 'login']);
+Route::post('/login', [UserController::class, 'login']); //Авторизация
+Route::group(['middleware' => ['auth:sanctum', 'role:user']], function () {
+    Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
+    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::get('/find_tests', [UserController::class, 'searchForAnExpert']); //Получение тестов с открытым доступом
+    Route::post('/accept_result', [UserController::class, 'postResultTest']); //Добавление результатов прохождения теста
+});
+Route::group(['middleware' => ['auth:sanctum', 'role:expert']], function () {
+    Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
+    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::get('/find_tests', [UserController::class, 'searchForAnExpert']); //Получение тестов с открытым доступом
+    Route::post('/accept_result', [UserController::class, 'postResultTest']); //Добавление результатов прохождения теста
+});
 
-// });
-// Route::group(['middleware' => 'role:expert'], function( ){
-//     Route::post('/signin', [UserController::class, 'login']);
-
-// });
-// Route::group(['middleware' => 'role:admin'], function( ){
-//     Route::post('/signin', [UserController::class, 'login']);
-
-// });
-// Route::group(['middleware' => 'role:teacher'], function( ){
-//     Route::post('/signin', [UserController::class, 'login']);
-
-// });
+Route::group(['middleware' => ['auth:sanctum', 'role:teacher']], function () {
+    Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
+    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::post('create_new_expert', [UserController::class, 'createExpert']); //Выдача прав доступа "Эксперт"
+    Route::post('/give_acces_expert', [UserController::class, 'addingAccessToTest']); //Открытие доступа к тесту
+    Route::get('/get_all_expert', [UserController::class, 'getAllExpert']); //Получение всех экспертов
+    Route::get('/all_tests', [UserController::class, 'getAllTests']); //Получение всех тестов
+    Route::post('/create_test', [UserController::class, 'postTests']); //Добавление тестов 
+    Route::post('/create_subject', [UserController::class, 'createSubject']); //Добавление предмета
+    Route::get('/find_for_admin', [UserController::class, 'findForAdmin']); //Поиск по почте
+});
+Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () {
+    Route::get('/find_for_admin', [UserController::class, 'findForAdmin']); //Поиск по почте
+    Route::post('/create_subject', [UserController::class, 'createSubject']); //Добавление предмета
+    Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
+    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::post('create_new_teacher', [UserController::class, 'createTeacher']); //Выдача прав доступа "Учитель"
+    Route::get('/all_tests', [UserController::class, 'getAllTests']); //Получение всех тестов
+    Route::post('create_new_expert', [UserController::class, 'createExpert']); //Выдача прав доступа "Эксперт"
+    Route::post('/give_acces_expert', [UserController::class, 'addingAccessToTest']); //Открытие доступа к тесту
+    Route::post('/create_test', [UserController::class, 'postTests']); //Добавление тестов 
+});
