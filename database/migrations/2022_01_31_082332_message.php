3b1bae15c11b26be67ba629d1e571cb0663696df
AlphTheMech <76548040+AlphTheMech@users.noreply.github.com>
2022-02-16 17:56:51 +0400

    init

--- database/migrations/2022_01_31_082332_message.php
+++ database/migrations/2022_01_31_082332_message.php
@@ -13,17 +13,18 @@ class Message extends Migration
      */
     public function up()
     {
-        // Schema::create('messages', function(Blueprint $table){
-        //     $table->id();
-        //     $table->unsignedBigInteger('dialog_id');
-        //     // $table->integer('bucket');
-        //     $table->unsignedBigInteger('message_id');
-        //     $table->unsignedBigInteger('author_id');
-        //     $table->string('content');
-        //     $table->timestamps();
-        //     $table->foreign();
-        //     // $table->primary(['message_id', 'author_id']);
-        // });
+        Schema::create('messages', function(Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('dialog_id');
+            // $table->integer('bucket');
+            $table->unsignedBigInteger('message_id');
+            $table->unsignedBigInteger('author_id');
+            $table->string('content');
+            $table->timestamps();
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->foreign('dialog_id')->references('id')->on('dialog');
+            // $table->primary(['message_id', 'author_id']);
+        });
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -25,36 +25,36 @@ Route::get('/dowload_file', [ImageController::class, 'donwload']); //Скачи�
 Route::post('/login', [UserController::class, 'login']); //Авторизация
 Route::group(['middleware' => ['auth:sanctum', 'role:user']], function () {
     Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
-    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
-    Route::get('/find_tests', [UserController::class, 'searchForAnExpert']); //Получение тестов с открытым доступом
-    Route::post('/accept_result', [UserController::class, 'postResultTest']); //Добавление результатов прохождения теста
+    Route::post('/user/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::get('/user/find_tests', [UserController::class, 'searchForAnExpert']); //Получение тестов с открытым доступом
+    Route::post('/user/accept_result', [UserController::class, 'postResultTest']); //Добавление результатов прохождения теста
 });
 Route::group(['middleware' => ['auth:sanctum', 'role:expert']], function () {
-    Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
-    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
-    Route::get('/find_tests', [UserController::class, 'searchForAnExpert']); //Получение тестов с открытым доступом
+    Route::get('/expert/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
+    Route::post('/expert/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::get('/expert/find_tests', [UserController::class, 'searchForAnExpert']); //Получение тестов с открытым доступом
     // Route::post('/accept_result', [UserController::class, 'postResultTest']); //Добавление результатов прохождения теста
 });
 
 Route::group(['middleware' => ['auth:sanctum', 'role:teacher']], function () {
-    Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
-    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
-    Route::post('create_new_expert', [UserController::class, 'createExpert']); //Выдача прав доступа "Эксперт"
-    Route::post('/give_acces_expert', [UserController::class, 'addingAccessToTest']); //Открытие доступа к тесту
-    Route::get('/get_all_expert', [UserController::class, 'getAllExpert']); //Получение всех экспертов
-    Route::get('/all_tests', [UserController::class, 'getAllTests']); //Получение всех тестов
-    Route::post('/create_test', [UserController::class, 'postTests']); //Добавление тестов 
-    Route::post('/create_subject', [UserController::class, 'createSubject']); //Добавление предмета
-    Route::get('/find_for_admin', [UserController::class, 'findForAdmin']); //Поиск по почте
+    Route::get('/teacher/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
+    Route::post('/teacher/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::post('/teacher/create_new_expert', [UserController::class, 'createExpert']); //Выдача прав доступа "Эксперт"
+    Route::post('/teacher/give_acces_expert', [UserController::class, 'addingAccessToTest']); //Открытие доступа к тесту
+    Route::get('/teacher/get_all_expert', [UserController::class, 'getAllExpert']); //Получение всех экспертов
+    Route::get('/teacher/all_tests', [UserController::class, 'getAllTests']); //Получение всех тестов
+    Route::post('/teacher/create_test', [UserController::class, 'postTests']); //Добавление тестов 
+    Route::post('/teacher/create_subject', [UserController::class, 'createSubject']); //Добавление предмета
+    Route::get('/teacher/find_for_admin', [UserController::class, 'findForAdmin']); //Поиск по почте
 });
 Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () {
-    Route::get('/find_for_admin', [UserController::class, 'findForAdmin']); //Поиск по почте
-    Route::post('/create_subject', [UserController::class, 'createSubject']); //Добавление предмета
-    Route::get('/user/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
-    Route::post('/image', [ImageController::class, 'create']); //Загрузка фото
-    Route::post('create_new_teacher', [UserController::class, 'createTeacher']); //Выдача прав доступа "Учитель"
-    Route::get('/all_tests', [UserController::class, 'getAllTests']); //Получение всех тестов
-    Route::post('create_new_expert', [UserController::class, 'createExpert']); //Выдача прав доступа "Эксперт"
-    Route::post('/give_acces_expert', [UserController::class, 'addingAccessToTest']); //Открытие доступа к тесту
-    Route::post('/create_test', [UserController::class, 'postTests']); //Добавление тестов 
+    Route::get('/admin/find_for_admin', [UserController::class, 'findForAdmin']); //Поиск по почте
+    Route::post('/admin/create_subject', [UserController::class, 'createSubject']); //Добавление предмета
+    Route::get('/admin/info', [UserController::class, 'getInfoUser']);//Получение информации о пользователе
+    Route::post('/admin/image', [ImageController::class, 'create']); //Загрузка фото
+    Route::post('/admin/create_new_teacher', [UserController::class, 'createTeacher']); //Выдача прав доступа "Учитель"
+    Route::get('/admin/all_tests', [UserController::class, 'getAllTests']); //Получение всех тестов
+    Route::post('/admin/create_new_expert', [UserController::class, 'createExpert']); //Выдача прав доступа "Эксперт"
+    Route::post('/admin/give_acces_expert', [UserController::class, 'addingAccessToTest']); //Открытие доступа к тесту
+    Route::post('/admin/create_test', [UserController::class, 'postTests']); //Добавление тестов 
 });
