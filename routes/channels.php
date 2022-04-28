bf08a58c88d842db07561613891bf4f34c1dba0d
AlphTheMech <76548040+AlphTheMech@users.noreply.github.com>
2022-04-28 22:08:50 +0400

    ой я тут накосячил

--- routes/channels.php
+++ routes/channels.php
@@ -15,18 +15,18 @@ use App\Models\Session;
 */
 
 
-       // Broadcast::channel('App.User.{id}', function ($user, $id) {
-   // return (int) $user->id === (int) $id;
-//});
+Broadcast::channel('App.User.{id}', function ($user, $id) {
+   return (int) $user->id === (int) $id;
+});
 
-//Broadcast::channel('Chat', function ($user) {
- //   return $user;
-//});
+Broadcast::channel('Chat', function ($user) {
+   return $user;
+});
 
 
-//Broadcast::channel('Chat.{session}', function ($user, Session $session) {
-  //  if ($user->id  === $session->user1_id || $user->id  === $session->user2_id) {
-   //     return true;
-   // }
-   // return false;
-//});
+Broadcast::channel('Chat.{session}', function ($user, Session $session) {
+   if ($user->id  === $session->user1_id || $user->id  === $session->user2_id) {
+      return true;
+   }
+   return false;
+});
